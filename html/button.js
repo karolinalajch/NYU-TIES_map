add236c416f732b8b8031bfd140464a4f70a6d13
Hillary Gao <hg1240@nyu.edu>
2023-05-17 14:50:38 -0400

    names

--- html/button.js
+++ html/button.js
@@ -17,6 +17,25 @@ function toggle(button) {
   }
 }
 
+function toggle_dropdown(button) {
+
+  // this works because the button is immediately after the "moreDetails" element it pertains to
+  let Text = button.previousElementSibling;
+  let btnText = button;
+
+  // this would work if you move the button so it is not immediately after moreDetails, but still in the same parent div.
+  //let Text = button.parentElement.querySelector(".moreDetails");
+
+  if (Text.style.display == "none") {
+    Text.style.display = "block";
+    btnText.innerHTML = "↑";
+  }
+  else {
+    Text.style.display = "none";
+    btnText.innerHTML = "↓";
+  }
+}
+
 const moreDetailses = document.querySelectorAll(".moreDetails");
 for (let i = 0; i < moreDetailses.length; i++) {
   moreDetailses[i].style.display = "none";
